d3ead4b2aab957b468eb19adb7ea57a1d580f8c9
harrifano1997 <richanne.harri@gmail.com>
2025-07-26 05:45:20 +0800

    Updated HTML and Javascript for Animation
    Animated the Aspiring Full Stack Developer

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const typed = new Typed(".name",{
-    strings: ["Harri Fano", "Aspiring Full Stack Developer"],
-    typeSpeed : 150,
-    backSpeed : 150,
-    loop : true
-})
+const headerName = document.querySelector(".pAnimate");
+const text       = "Aspiring Full Stack Developer";
+
+function textTypingEffect(element, text, index = 0) {
+  if (index < text.length) {
+    element.textContent += text[index];
+    setTimeout(() => {
+      textTypingEffect(element, text, index + 1);
+    }, 300);
+  } else {
+    setTimeout(() => {
+      element.textContent = "";               
+      textTypingEffect(element, text, 0);     
+    }, 1500); 
+  }
+}
+textTypingEffect(headerName, text);
